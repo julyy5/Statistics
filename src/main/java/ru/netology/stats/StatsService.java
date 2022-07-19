34ddfd84e83827ff884c4327eae1de86bdea938d
julyy5 <smirnova-1999@inbox.ru>
2022-07-19 14:01:46 +0300

    Расчёт суммы, при помощи цикла

--- src/main/java/ru/netology/stats/StatsService.java
+++ src/main/java/ru/netology/stats/StatsService.java
@@ -2,14 +2,18 @@ package ru.netology.stats;
 
 public class StatsService {
     public long sumOfSales(long[] sales) {
-        long sum = sales[0] + sales[1] + sales[2] + sales[3] + sales[4] + sales[5] + sales[6] + sales[7] + sales[8] + sales[9] + sales[10] + sales[11];
+        int sum=0;
+        for (long sale:sales
+             ) {
+            sum += sale;
+
+        }
         return sum;
     }
 
     public long mediumSaleOfMonth(long[] sales) {
-        long sum = (sales[0] + sales[1] + sales[2] + sales[3] + sales[4] + sales[5] + sales[6] + sales[7] + sales[8] + sales[9] + sales[10] + sales[11]) / 12;
-        return sum;
-    }
+        return sumOfSales(sales)/12;
+            }
 
     public long maxSales(long[] sales) {
         int maxMonth = 0;
@@ -36,10 +40,9 @@ public class StatsService {
     }
 
     public long minOfMedium(long[] sales) {
-        long sum = (sales[0] + sales[1] + sales[2] + sales[3] + sales[4] + sales[5] + sales[6] + sales[7] + sales[8] + sales[9] + sales[10] + sales[11]) / 12;
         int count = 0;
         for (long sale : sales) {
-            if (sale < sum) {
+            if (sale < mediumSaleOfMonth(sales)) {
                 count++;
             }
         }
@@ -47,10 +50,9 @@ public class StatsService {
     }
 
     public int maxOfMedium(long[] sales) {
-        long sum = (sales[0] + sales[1] + sales[2] + sales[3] + sales[4] + sales[5] + sales[6] + sales[7] + sales[8] + sales[9] + sales[10] + sales[11]) / 12;
         int count = 0;
         for (long sale : sales) {
-            if (sale > sum) {
+            if (sale > mediumSaleOfMonth(sales)) {
                 count++;
             }
         }
